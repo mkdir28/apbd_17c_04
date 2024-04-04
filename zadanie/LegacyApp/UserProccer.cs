a8d3a186b463423e704452d743f2c5833254ff80
Maryna Kamienieva <mkdirwd@gmail.com>
2024-04-04 17:57:03 +0200

    update UserProccer.cs change to switch case

--- zadanie/LegacyApp/UserProccer.cs
+++ zadanie/LegacyApp/UserProccer.cs
@@ -5,27 +5,25 @@ public class UserProccer
 {
     public void ClientType(User user)
     {
-        if (user.Client.Type == "VeryImportantClient")
+        switch (user.Client.Type)
         {
-            user.HasCreditLimit = false;
-        }
-        else if (user.Client.Type == "ImportantClient")
-        {
-            using (var userCreditService = new UserCreditService())
-            {
-                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                creditLimit = creditLimit * 2;
-                user.CreditLimit = creditLimit;
-            }
-        }
-        else
-        {
-            user.HasCreditLimit = true;
-            using (var userCreditService = new UserCreditService())
-            {
-                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                user.CreditLimit = creditLimit;
-            }
+            case "VeryImportantClient":
+                user.HasCreditLimit = false;
+                break;
+            case "ImportantClient":
+                using (var userCreditService = new UserCreditService()) {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+                break;
+            default:
+                user.HasCreditLimit = true; 
+                using (var userCreditService = new UserCreditService()) {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+                break;
         }
     }
 }
