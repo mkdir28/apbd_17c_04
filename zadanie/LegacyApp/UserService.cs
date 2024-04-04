a2ed1167b120578e1f8c5b7735e26090f8aafea9
Maryna Kamienieva <mkdirwd@gmail.com>
2024-04-04 17:49:07 +0200

    update UserService.cs class

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -2,7 +2,7 @@
 
 namespace LegacyApp
 {
-    public class UserService
+    public class UserService: UserProccer
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
@@ -37,28 +37,8 @@ namespace LegacyApp
                 LastName = lastName
             };
 
-            if (client.Type == "VeryImportantClient")
-            {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            UserProccer proccer = new UserProccer();
+            proccer.ClientType(user);
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
